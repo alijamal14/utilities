d6d559f923be9762c77de8c98c7ed8d86ed06e3a
alijamal14 <alijamal14@gmail.com>
2016-10-31 14:50:56 +0500

    New Function Addition And Fixed
    Salaamun Alekum
    
    [Added] SelectDropDownTextOption, TakeOnlyInputNumbers, CapitalizeText
    [Fixed] PascalCaseSpacesText, PascalCaseSpacesElement
    
    Thank You

--- Utilities.js
+++ Utilities.js
@@ -1,4 +1,20 @@
 ﻿function Utils() {
+    this.SelectDropDownTextOption = function (Selector, StringToSelectDropDownTextOption) {
+        if (Utilities.IsNotUndefinedOrNull(Selector) && Utilities.IsNotUndefinedOrNull(StringToSelectDropDownTextOption)) {
+            $(Selector).find('option').each(function (Index, Element) {
+                if ($(Element).text() == StringToSelectDropDownTextOption) {
+                    console.log($(Element).text() + " " + $(Element).val())
+                    $(Selector).val($(Element).val())
+                    return false;
+                }
+            })
+        }
+    },
+    this.ScrollTo = function (CSSSelectorOrElement) {
+        $('html, body').animate({
+            scrollTop: $(CSSSelectorOrElement).first().offset().top - 35
+        }, 30);
+    },
     this.Isfunction = function (ObjectReceived) {
         return typeof ObjectReceived === "function"
     },
@@ -16,6 +32,18 @@
         }
 
     },
+    this.TakeOnlyInputNumbers = function (event) {
+        var keycode = event.which;
+        if (!(event.shiftKey == false && (keycode == 46 || keycode == 8 || keycode == 37 || keycode == 39 || (keycode >= 48 && keycode <= 57)))) {
+            $(this).siblings('.err').text('Only Numbers are allowed')
+            $(this).siblings('.err').css('display', '')
+            event.preventDefault();
+        }
+        else {
+            $(this).siblings('.err').text('')
+            $(this).siblings('.err').css('display', 'none')
+        }
+    },
     this.IsEven = function (n) {
         return n % 2 == 0;
     },
@@ -24,7 +52,7 @@
     },
     this.DateTimeClock = function (Selector) {
 
-        
+
         setInterval(function () {
             if (Utilities.IsEven(moment().format("ss"))) {
 
@@ -53,8 +81,8 @@
             if (objectExcludedFieldsSelectors != undefined && (objectExcludedFieldsSelectors.indexOf($(this).attr('name')) > -1)) {
                 return true
             }
-            else if (($('[name="' + $(this).attr('name') + '"]').length > 1)&&($('[name="' + $(this).attr('name') + '"]').prop('tagName').toUpperCase() == "INPUT" && $('[name="' + $(this).attr('name') + '"]').prop('type').toUpperCase() == "RADIO")) {
-                    Data[$(this).attr('name')] = $('[name="' + $(this).attr('name') + '"]:checked').val()
+            else if (($('[name="' + $(this).attr('name') + '"]').length > 1) && ($('[name="' + $(this).attr('name') + '"]').prop('tagName').toUpperCase() == "INPUT" && $('[name="' + $(this).attr('name') + '"]').prop('type').toUpperCase() == "RADIO")) {
+                Data[$(this).attr('name')] = $('[name="' + $(this).attr('name') + '"]:checked').val()
             }
             else {
                 Data[$(this).attr('name')] = $(this).val()
@@ -70,7 +98,17 @@
                         // uppercase the first character
                         .replace(/^./, function (str) { return str.toUpperCase(); }).split(' ')).each(function (Index, Element) {
                             //console.log(Index + ". " + Element + " " + Element.length)
-
+                            if (Utilities.IsNotUndefinedOrNull(Element)) {
+                                var CaptializeString = ''
+                                for (var i = 0; i < Element.length; i++) {
+                                    if (i == 0) {
+                                        CaptializeString += Element[i].toUpperCase()
+                                    }
+                                    else
+                                        CaptializeString += Element[i]
+                                }
+                                Element = CaptializeString
+                            }
                             if (Element.length < 1) {
 
                             }
@@ -91,7 +129,17 @@
                         // uppercase the first character
                         .replace(/^./, function (str) { return str.toUpperCase(); }).split(' ')).each(function (Index, Element) {
                             ///console.log(Index + ". " + Element + " " + Element.length)
-
+                            if (Utilities.IsNotUndefinedOrNull(Element)) {
+                                var CaptializeString = ''
+                                for (var i = 0; i < Element.length; i++) {
+                                    if (i == 0) {
+                                        CaptializeString += Element[i].toUpperCase()
+                                    }
+                                    else
+                                        CaptializeString += Element[i]
+                                }
+                                Element = CaptializeString
+                            }
                             if (Element.length < 1) {
 
                             }
@@ -108,6 +156,45 @@
         //        .replace(/([A-Z])/g, ' $1')
         //        // uppercase the first character
         //        .replace(/^./, function (str) { return str.toUpperCase(); }))
+    },
+    this.CapitalizeText = function (Text) {
+        var NewString = ""
+        $(Text.trim()
+                        // uppercase the first character
+                        .replace(/^./, function (str) { return str.toUpperCase(); }).split(' ')).each(function (Index, Element) {
+                            //console.log(Index + ". " + Element + " " + Element.length)
+                            if (Utilities.IsNotUndefinedOrNull(Element)) {
+                                var CaptializeString = ''
+                                for (var i = 0; i < Element.length; i++) {
+                                    if (i == 0) {
+                                        CaptializeString += Element[i].toUpperCase()
+                                    }
+                                    else
+                                        CaptializeString += Element[i]
+                                }
+                                Element = CaptializeString
+                            }
+                            if (Element.length < 1) {
+
+                            }
+                            else if (Element.length == 1) {
+                                NewString += Element
+                            }
+                            else {
+                                NewString += " " + Element
+                            }
+                        })
+        return NewString
+    },
+    this.RecursiveCapitalizeTextValue = function (Selector) {
+        if (Utilities.IsUndefinedOrNull(Selector)) {
+            Selector = 'input.capitalize, textarea.capitalize'
+        }
+        $('form#patient').find(Selector).each(function (index, element) {
+
+            $(element).val(Utilities.CapitalizeText($(element).val()).trim())
+
+        })
     },
     this.CalculateAge = function (FirstMoment, SecondMoment) {
         if (SecondMoment == undefined || SecondMoment == null || SecondMoment == '') {
